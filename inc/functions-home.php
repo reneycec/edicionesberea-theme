a90155025121ddafef461f11134b48d77e4e5be4
reneycec <reneycec@gmail.com>
2026-09-15 19:58:54 -0600

    fix: romper cache CSS con filemtime + zoom hover en portadas + overlay hero reforzado

--- inc/functions-home.php
+++ inc/functions-home.php
@@ -35,7 +35,7 @@ add_action( 'wp_enqueue_scripts', function () {
 		'libreria-home',
 		get_stylesheet_directory_uri() . '/assets/css/home.css',
 		array( 'storefront-style' ),
-		wp_get_theme()->get( 'Version' )
+		filemtime( get_stylesheet_directory() . '/assets/css/home.css' )
 	);
 
 	wp_enqueue_script(
